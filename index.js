05cb281abc0aee1ad424734cb47853468479d001
favoured mwange <mwangefavoured@gmail.com>
2024-07-11 23:03:09 +0300

    adds post

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Add your code here
+
+function submitData(name, email){
+    const formData={
+        name:name,
+        email:email
+    }
+    const configurationObject ={
+        method:"POST",
+        headers:{
+            "content-Type":"application/json",
+            "accept":"application/json",
+        },
+        body:JSON.stringify(formData)
+    }
+     return fetch("http://localhost:3000/users",configurationObject)
+    .then(function(response){
+        return response.json()
+    })
+    .then(function(data){
+        let h2=document.createElement('h2')
+        h2.innerHTML=data.id
+        document.body.appendChild(h2)
+    
+    })
+    .catch(function(error){
+        let h3=document.createElement('h3')
+        h3.innerHTML=error.message
+        document.body.appendChild(h3)
+    })
+    }
